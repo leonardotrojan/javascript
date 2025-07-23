94ab68bb9352cf5ca0e54faf300fb0db274e8afb
Leonardo Trojan <leo.troyan.melo@gmail.com>
2025-07-23 15:51:28 -0300

    avançando nos estudos

---
+++ exercícios II/aula003/js/script.js
@@ -0,0 +1,11 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+console.LOG("Mais um console.log"); //errado. Case Sensative
+
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
---
+++ exercícios II/aula004/js/scripts.js
@@ -0,0 +1,29 @@
+var teste = 1;
+
+nome = "João";
+
+console.log(teste);
+
+teste = 'Matheus';
+
+console.log(teste);
+
+console.log(nome);
+
+var meuPrimeiroNome = 'Leo';
+
+console.log(meuPrimeiroNome); // camelCase
+
+let testanho = 1;
+
+const ola = 2;
+
+console.log(testanho);
+
+console.log(ola);
+
+var meuNome;
+
+meuNome = "Matheus";
+
+console.log(meuNome);
