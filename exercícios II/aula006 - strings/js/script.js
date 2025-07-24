948b2143ece75c707852b39e509c4011ecbf1ae3
Leonardo Trojan <leo.troyan.melo@gmail.com>
2025-07-24 17:40:52 -0300

    botando nome nas pastas

---
+++ exercícios II/aula006 - strings/js/script.js
@@ -0,0 +1,21 @@
+var nome = 2;
+var sobrenome = "Trojan";
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(nomeCompleto);
+
+console.log(typeof "5.292929");
+
+var frase = 'Essa é uma frase complexa';
+
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.writeln("Ele disse: 'olá'");
+
+console.log("Este número: " + nome);
