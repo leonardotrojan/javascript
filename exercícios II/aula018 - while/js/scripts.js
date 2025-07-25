a1eb0ca5ba316a2c6617d7ff2acf63c5388a2ad2
Leonardo Trojan <leo.troyan.melo@gmail.com>
2025-07-25 01:56:16 -0300

    finalizando por hoje

---
+++ exercícios II/aula018 - while/js/scripts.js
@@ -0,0 +1,26 @@
+var x = 0;
+
+while(x < 5) {
+    console.log("Testando repeticão " + x);
+
+    // incrementador
+    x++;
+}
+
+var arr = ['teste', 'testando', 'a', 'b'];
+var y = 0;
+
+while (y <= 3) {
+    
+    console.log(arr[y]);
+
+    y++
+}
+
+var palavra = "Leonardo";
+var i = 0;
+
+while (i <= 7) {
+    console.log(palavra[i])
+    i++
+}
